c10fbf4190c5c60915c56230be4a4b9e676ae2ff
hossein yaghoubi <37728234+hypy13@users.noreply.github.com>
2024-05-13 19:43:51 +0330

    Update model_fields.py
    fix date widget import

--- jalali_date_new/model_fields.py
+++ jalali_date_new/model_fields.py
@@ -1,6 +1,6 @@
 from django.db.models import DateTimeField, DateField
 from jalali_date_new.fields import JalaliDateTimeField, JalaliDateField
-from jalali_date_new.widgets import AdminJalaliDateTimeWidget, AdminJalaliTimeWidget
+from jalali_date_new.widgets import AdminJalaliDateTimeWidget, AdminJalaliDateWidget
 
 
 class JalaliDateTimeModelField(DateTimeField):
@@ -19,7 +19,7 @@ class JalaliDateModelField(DateField):
         return super().formfield(
             **{
                 'form_class': JalaliDateField,
-                "widget": AdminJalaliTimeWidget,
+                "widget": AdminJalaliDateWidget,
                 **kwargs,
             }
         )
